4ec7476d04d22aaca918098a526da7d3b0a7e878
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-04-11 18:48:17 +0300

    add

--- Memory Game(Ania)/script.js
+++ Memory Game(Ania)/script.js
@@ -150,3 +150,13 @@ document.addEventListener('DOMContentLoaded', () => {
   
     createBoard();
   });
+
+  $('.menu a').click(function(){
+ 
+    $('.trigger').toggle();
+    $('.menu').toggleClass('round');
+    $('.close').toggle();
+    $('.drop-down').toggleClass('down');
+    
+   
+  });
