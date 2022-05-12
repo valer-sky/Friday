2ecca61088f4e8683762a88eb6904ba23605c6d2
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-05-12 15:09:58 +0300

    add ajax

--- MeMo/index.js
+++ MeMo/index.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 let toggleButton = document.querySelector('.toggle-menu');
 let navBar = document.querySelector('.nav-bar');
 toggleButton.addEventListener('click', function () {
@@ -57,16 +57,23 @@ gameField.onclick = function(e) {
 
   // clear openedCards array for next try
   openedCards = [];
-  let resultDisplay = document.querySelector('.result');
+  // let resultDisplay = document.querySelector('.result');
   
-
   if (complitedCards === cardsContent.length) {
-    resultDisplay.textContent = `Congratulations! You found them all! Your time: ${timerUI.innerHTML} and your steps: ${stepsCounter.innerHTML}`;
+    playerName();
+    
+  //   let namePlayer;
+  // namePlayer = prompt(`Congratulations! You found them all! Your time: ${timerUI.innerHTML} and your steps: ${stepsCounter.innerHTML}. Введите ваше имя для таблицы рекордов', ''`);
+  // console.log(namePlayer);
+    // resultDisplay.textContent = `Congratulations! You found them all! Your time: ${timerUI.innerHTML} and your steps: ${stepsCounter.innerHTML}`;
+    // alert(`Congratulations! You found them all! Your time: ${timerUI.innerHTML} and your steps: ${stepsCounter.innerHTML}`);
     restartButton.hidden = false;
+
   }
 }
 
-restart.onclick = function() {
+restartButton.onclick = function() {
+  
   gameField.innerHTML = '';
   cardsContent = mixarr(cardsContent);
   // create cards in HTML
@@ -75,10 +82,11 @@ restart.onclick = function() {
     gameField.lastElementChild.innerHTML =  '<img class="card-image" src="' + cardsContent[i].getAttribute('src') + '">';
   }
   toggleTimer();
+  // resultDisplay.textContent.hidden = true;
   stepsCounter.innerHTML = 0;
   complitedCards = 0;
   restartButton.hidden = true;
-  resultDisplay.textContent = 'hidden';
+  // resultDisplay.textContent = 'hidden';
 }
 
 function openCard(target) {
@@ -158,52 +166,195 @@ function toggleTimer() {
   }
 }
 
+let hashStorage = {};
+function TAJAXStorage() {
+	TAJAXStorage.prototype.addValue = function(key, value) {
+        hashStorage[key] = value;
+    };
+    TAJAXStorage.prototype.getValue = function(key) {
+		return hashStorage[key];
+	};
 
-window.onhashchange = renderNewState;
-function renderNewState() {
-    const hash = window.location.hash;
-    let state = decodeURIComponent(hash.substr(1));
+	TAJAXStorage.prototype.deleteValue = function(key) {
+		if (!(key in hashStorage)) {
+			return false;
+	    }
+    };
+    TAJAXStorage.prototype.getKeys = function() {
+		return Object.keys(hashStorage);
+    };
+}	
 
-    if (state === '') {
-        state = {page: 'first'};
+function DrinkAjaxStorage() {
+    this.setLoc = function () {
+        storeInfo();
+    };
+}
+
+function restoreInfo() {
+    $.ajax({
+        url: "https://fe.it-academy.by/AjaxStringStorage2.php",
+        type: 'POST',
+        cache: false,
+        dataType: 'json',
+        data: { f: 'READ', n: 'STANKEVICH_VALERY_AJAX_STORAGE_GAME' },
+        success: readReady,
+        error: errorHandler
+    });
+}
+
+function readReady(callresult) {
+    if (callresult.error != undefined) {
+        alert(callresult.error);
+    } else if (callresult.result != "") {
+        hashStorage = JSON.parse(callresult.result);
+    }
+}
+restoreInfo();
+
+var updatePassword;
+
+function storeInfo() {
+    updatePassword = Math.random();
+    $.ajax({
+        url: "https://fe.it-academy.by/AjaxStringStorage2.php",
+        type: 'POST',
+        cache: false,
+        dataType: 'json',
+        data: { f: 'LOCKGET', n: 'STANKEVICH_VALERY_AJAX_STORAGE_GAME', p: updatePassword },
+        success: lockGetReady,
+        error: errorHandler
+    });
+}
+
+function lockGetReady(callresult) {
+    if (callresult.error != undefined) {
+        alert(callresult.error);
     } else {
-        state = JSON.parse(state);
+        let info = hashStorage;
+        $.ajax({
+            url: "https://fe.it-academy.by/AjaxStringStorage2.php",
+            type: 'POST',
+            cache: false,
+            dataType: 'json',
+            data: { f: 'UPDATE', n: 'STANKEVICH_VALERY_AJAX_STORAGE_GAME', v: JSON.stringify(info), p: updatePassword },
+            success: updateReady,
+            error: errorHandler
+        });
     }
+}
 
-    let page = '';
-
-    switch(state.page) {
-        case 'first':
-            page += 'rrrr';
-            break;
-        case 'second':
-            page += `<ol>
-                                            <li>Ищи совпадающие по цвету элементы по горизонтали, вертикали или диагонали (3 и более)</li><br>
-                                            <li>Элементами можно управлять при помощи мыши на ПК и пальцем на тачскрине</li><br>
-                                            <li>Нажми на элемент и проведи в сторону второго элемента, с которым необходимо поменяться местами</li><br>
-                                            <li>Одинаковые группы исчезнут и поле перестоится, а ты получишь очки</li><br>
-                                            <li>Набери максимальное количество очков за 30 ходов</li>
-                                        </ol>`;
-            break;
-        case 'third':
-            page += '<h1>Таблица рекордов</h1>';
-            break;
+function updateReady(callresult) {
+    if (callresult.error != undefined) {
+        alert(callresult.error);
     }
-
-    document.getElementById('page').innerHTML = page;
 }
 
-function switchToState(state) {
-    location.hash = encodeURIComponent(JSON.stringify(state));
-}
-function switchToFirst() {
-    switchToState({page: 'first'});
-}
-function switchToSecond() {
-    switchToState({page: 'second'});
-}
-function switchToThird() {
-    switchToState({page: 'third'});
+function errorHandler(jqXHR, statusStr, errorStr) {
+    alert(statusStr + ' ' + errorStr);
 }
 
-renderNewState();
+var drinkStorage = new TAJAXStorage();
+var drinksAjaxStorage = new DrinkAjaxStorage();
+
+var drinkName = document.getElementById('drinkName');
+
+function playerName() {
+	var player;
+	var results = {};
+	player = prompt("напишите ваше имя");
+	results.player = player;
+	results.fullTime = `${timerUI.innerHTML}`;
+	results.fullSteps = `${stepsCounter.innerHTML}`;
+	console.log(results);
+	drinkStorage.addValue(player, results);
+    drinksAjaxStorage.setLoc();
+
+	console.log(drinkStorage);
+  var drinkList = document.getElementById("drinkList");
+
+  drinkList.onclick = function() {
+	var drinkInfoP = document.getElementById("drinkInfoP");
+		drinkInfoP.innerHTML = results;
+};  
+};
+
+// var drinkInfo = document.getElementById("drinkInfo");
+
+// drinkInfo.onclick = function() {
+// 	var drinkInf = prompt("Напишите название напитка");
+// 	var drinkInfoP = document.getElementById("drinkInfoP");
+
+// 	var answer = drinkStorage.getValue(drinkInf);
+// 		if (drinkStorage.getValue(drinkInf) !== undefined) {
+// 			drinkInfoP.innerHTML = "напиток: " + drinkInf + 
+// 								   "<br>" + "алкогольный: " + (answer.fullTime === true ? "да" : "нет") + 
+// 								   "<br>" + "Рецепт: " + (answer.recipe ? answer.recipe : "к сожалению РЕЦЕПТА НЕТ");
+// 		} else {
+// 			drinkInfoP.innerHTML = "В хранилище такой напиток ОТСУТСТВУЕТ!";
+// 	}
+// };
+
+// var drinkInfoDel = document.getElementById("drinkInfoDel");
+
+// drinkInfoDel.onclick = function() {
+// 	var drinkInfoDel = prompt("Напишите название напитка");
+// 	var drinkInfoP = document.getElementById("drinkInfoP");
+// 		if (drinkStorage.deleteValue(drinkInfoDel) === true) {
+// 			drinkInfoP.innerHTML = "информация о напитке УДАЛЕНО!";
+//             drinksAjaxStorage.setLoc();
+// 		} else {
+// 			drinkInfoP.innerHTML = "В хранилище такой напиток отсутствует";
+// 	}
+// };
+
+
+
+// window.onhashchange = renderNewState;
+// function renderNewState() {
+//     const hash = window.location.hash;
+//     let state = decodeURIComponent(hash.substr(1));
+
+//     if (state === '') {
+//         state = {page: 'first'};
+//     } else {
+//         state = JSON.parse(state);
+//     }
+
+//     let page = '';
+
+//     switch(state.page) {
+//         case 'first':
+//             page += 'rrrr';
+//             break;
+//         case 'second':
+//             page += `<ol>
+//                                             <li>Ищи совпадающие по цвету элементы по горизонтали, вертикали или диагонали (3 и более)</li><br>
+//                                             <li>Элементами можно управлять при помощи мыши на ПК и пальцем на тачскрине</li><br>
+//                                             <li>Нажми на элемент и проведи в сторону второго элемента, с которым необходимо поменяться местами</li><br>
+//                                             <li>Одинаковые группы исчезнут и поле перестоится, а ты получишь очки</li><br>
+//                                             <li>Набери максимальное количество очков за 30 ходов</li>
+//                                         </ol>`;
+//             break;
+//         case 'third':
+//             page += '<h1>Таблица рекордов</h1>';
+//             break;
+//     }
+
+//     document.getElementById('page').innerHTML = page;
+// }
+
+// function switchToState(state) {
+//     location.hash = encodeURIComponent(JSON.stringify(state));
+// }
+// function switchToFirst() {
+//     switchToState({page: 'first'});
+// }
+// function switchToSecond() {
+//     switchToState({page: 'second'});
+// }
+// function switchToThird() {
+//     switchToState({page: 'third'});
+// }
+
+// renderNewState();
